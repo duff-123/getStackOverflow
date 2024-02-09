545f99334431f938ac30142e7c278a0f8f1ef285
duff-123 <140746225+duff-123@users.noreply.github.com>
2024-02-09 13:33:40 -0500

    Add basic css style and adjusted the url and params

--- getStackOverflow.py
+++ getStackOverflow.py
@@ -11,22 +11,51 @@ app = Flask(__name__)
 #cache.init_app(app)
 
 def get_recent_questions():
-    url = "https://api.stackexchange.com/2.3/questions"
+    url = "https://api.stackexchange.com/2.3/search/advanced"
+    params = {
+        "order": "desc",     # Most recent on top
+        "sort": "creation",  # Get most recently created
+        "accepted": "True",  # Filter for questions with accepted answers
+        "answers": 2,        # Filter for questions with at least 2 answers
+        "pagesize": 5,       # Limit the results
+        "site": "stackoverflow"
+    }
+    try:
+        response = requests.get(url, params=params)
+        response.raise_for_status()  # Error if not 200 status code
+        data = response.json()
+        return data['items']
+    except requests.exceptions.HTTPError as e:
+        raise Exception(f"Failed to retrieve data. Status code: {response.status_code}. Error: {str(e)}")
+
+def get_question(question_id):
+    url = f"https://api.stackexchange.com/2.3/questions/{question_id}"
     params = {
         "order": "desc",
-        "sort": "creation",
+        "sort": "activity",
         "site": "stackoverflow",
-        "pagesize": 5
+        "filter": "withbody"
     }
-    response = requests.get(url, params=params)
-    data = response.json()
-    return data['items']
+    try:
+        response = requests.get(url, params=params)
+        response.raise_for_status()  # Error if not 200 status code
+        data = response.json()
+        question = data['items'][0]
+        
+        question['body'] = BeautifulSoup(question['body'], 'html.parser').get_text()
+        question['body']=strip_tags(question['body'])
+        question['correct_answer_id'] = question.get('accepted_answer_id')  # Extract accepted answer ID
+        
+        return question
+    except requests.exceptions.HTTPError as e:
+        raise Exception(f"Failed to retrieve data. Status code: {response.status_code}. Error: {str(e)}")
+
 
 def get_answers(question_id):
     url = f"https://api.stackexchange.com/2.3/questions/{question_id}/answers"
     params = {
-        "order": "desc",
-        "sort": "votes",
+        #"order": "desc",
+        #"sort": "votes",
         "site": "stackoverflow",
         "filter": "withbody"
     }
